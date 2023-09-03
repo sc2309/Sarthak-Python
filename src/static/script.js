07af54e66a292d23814c52f70fd9de5ab8d638f3
Sarthak <sc2309>
2023-09-03 12:46:07 +0530

    printing the meanings

--- src/static/script.js
+++ src/static/script.js
@@ -10,8 +10,45 @@ $( document ).ready(function() {
             data: 'user_value='+field1Value,
             success: function(response) {
                 alert('entered')
-                console.log('response',response);
-                $("#result").text(response);
+                //console.log('response',response);
+                //$("#result").text(response);
+                
+                const indexA = 0;
+                const indexB = 2;
+                const indexC = 2;
+                const indexD = 1;
+                const indexE = 0;
+                const indexF = 1;
+
+                for (let a = 0; a < response.length; a++) {
+                    if (a === indexA) {
+                        for (let b = 0; b < response[a].length; b++) {
+                            if (b === indexB) {
+                                for (let c = 0; c < response[a][b].length; c++) {
+                                    if (c === indexC) {
+                                        for (let d = 0; d < response[a][b][c].length; d++) {
+                                            if (d === indexD) {
+                                                for (let e = 0; e < respo2nse[a][b][c][d].length; e++) {
+                                                    if (e === indexE) {
+                                                        for (let f = 0; f < response[a][b][c][d][e].length; f++) {
+                                                            if (f === indexF) {
+                                                                const WordMeaning = response[a][b][c][d][e][f][g];
+                                                                console.log("response:", WordMeaning);
+                                                                break;
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+                $("#result").text(WordMeaning);
             },
             error: function(error) {
                 console.log(error);
@@ -31,4 +68,8 @@ $( document ).ready(function() {
     }
 });
     });
-});*/
+});
+
+
+console.log(WordMeaning)//'response',WordMeaning)
+*/
